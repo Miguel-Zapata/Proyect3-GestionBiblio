5f46efcc5c15ac32a734bd10c400631f84bead86
Miguel Zapata <miguelzapata.disten@gmail.com>
2021-12-29 15:55:29 +0100

    server y json añadidos para heroku

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const cors = require('cors');
 
+// ... other imports HEROKU
+const path = require("path")
+
+
 // cloudinary
 require('cloudinary').config();
 const CLOUDINARY_URL = process.env.CLOUDINARY_URL;
@@ -20,6 +24,9 @@ const bookingRouter = require("./routes/Booking.Routes");
 const connectDB = require('./DB/connection.js');
 connectDB();
 
+// ... other app.use middleware HEROKU
+app.use(express.static(path.join(__dirname, "client", "build")))
+
 // Postman Frontend
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -32,6 +39,12 @@ app.use('/api/users', checkToken, userRouter);
 app.use('/api/libraries', checkToken, libraryRouter);
 app.use('/api/cards', cardRouter);
 app.use('/api/bookings', bookingRouter);
+
+
+// Right before your app.listen(), add this: HEROKU
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 // Puerto
 app.listen(PORT, () => { console.log(`listening on port ${PORT}`); });
